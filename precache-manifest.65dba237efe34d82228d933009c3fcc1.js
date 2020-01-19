b302c55d5a9c469f6303776ad26f9860fa556ac1
Eric Amrhein <eamrhein@protonmail.ch>
2020-01-18 22:21:32 -0800

    Updates

---
+++ precache-manifest.65dba237efe34d82228d933009c3fcc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54110b63072dae1e12204f75bd6fa7b0",
+    "url": "/stockresearch/index.html"
+  },
+  {
+    "revision": "2e9b29a6181006c9f5d1",
+    "url": "/stockresearch/static/css/main.f4d621d9.chunk.css"
+  },
+  {
+    "revision": "e347baaeaa764b2b835b",
+    "url": "/stockresearch/static/js/2.e702059e.chunk.js"
+  },
+  {
+    "revision": "9b499988666cc8d7d52d70a8c3b2f162",
+    "url": "/stockresearch/static/js/2.e702059e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e9b29a6181006c9f5d1",
+    "url": "/stockresearch/static/js/main.c7c2b074.chunk.js"
+  },
+  {
+    "revision": "e905e177f9d20aae9ac0",
+    "url": "/stockresearch/static/js/runtime-main.638360e1.js"
+  }
+]);
