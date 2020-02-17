ff454d8b5d7486bc3d5df531a9516275aee895d8
Eric Amrhein <eamrhein@protonmail.ch>
2020-02-16 19:12:02 -0800

    predeploy

--- src/components/stockPage.js
+++ src/components/stockPage.js
@@ -3,11 +3,14 @@ import { withRouter } from 'react-router-dom';
 import { fetchStockChart } from '../actions/stock_actions';
 import StockChart from './stockNFO';
 
-const mapSTP = (state, ownProps) => ({
-  stocks: state.stocks,
-  current: ownProps.match.params.sym || 'MSFT',
-  ownProps,
-});
+const mapSTP = (state, ownProps) => {
+  console.log(ownProps);
+  return {
+    stocks: state.stocks,
+    current: ownProps.match.params.sym || 'MSFT',
+    ownProps,
+  };
+};
 
 const mapDTP = (dispatch) => ({
   fetchStockChart: (ticker, range) => dispatch(fetchStockChart(ticker, range)),
