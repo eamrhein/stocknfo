de2091627e47e649db0ea4320f0ba66f641d7b25
Eric Amrhein <eamrhein@protonmail.ch>
2020-05-26 15:47:51 -0700

    fixes outdated dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import PropTypes from 'prop-types';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import Nav from './components/nav';
-import Main from './components/main';
-import { BaseStyle, theme } from './theme';
-
+import React from "react";
+import { Provider } from "react-redux";
+import PropTypes from "prop-types";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import Nav from "./components/nav";
+import Main from "./components/main";
+import { BaseStyle, theme } from "./theme";
 
 const App = ({ store }) => (
   <Provider store={store}>
     <ThemeProvider theme={theme}>
       <BaseStyle>
-        <Router basename={process.env.PUBLIC_URL}>
+        <Router>
           <Nav />
           <Main />
         </Router>
